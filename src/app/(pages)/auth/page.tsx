726d6646cd4c0117964f027b72a7e229480daf77
Ildar <ildar.latipov@gmail.com>
2025-07-17 21:52:34 +0200

    fix suspend

--- src/app/(pages)/auth/page.tsx
+++ src/app/(pages)/auth/page.tsx
@@ -1,15 +1,19 @@
 "use client";
-import { useState } from "react";
+
+import { useState, Suspense } from "react";
 import { SignInForm } from "@components/SignInForm";
 import { SignUpForm } from "@components/SignUpForm";
+import { Loading } from "@components/Loading/Loading";
 
 export default function AuthInPage() {
   const [form, setForm] = useState<"signin" | "signup">("signin");
 
   return (
     <main className=" mx-auto w-full md:w-[80%] flex-1 flex flex-col justify-center items-center">
-      {form === "signin" && <SignInForm setForm={setForm} />}
-      {form === "signup" && <SignUpForm setForm={setForm} />}
+      <Suspense fallback={<Loading />}>
+        {form === "signin" && <SignInForm setForm={setForm} />}
+        {form === "signup" && <SignUpForm setForm={setForm} />}
+      </Suspense>
     </main>
   );
 }
